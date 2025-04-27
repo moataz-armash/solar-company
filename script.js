3f6d6659c44e1651cb764289c981e7f8a774f663
moataz-armash <mo3taz314@gmail.com>
2025-04-27 06:34:59 +0300

    finish hero section with responsive

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const navLinks = document.getElementById("navbar-links");
 
 menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
 });
